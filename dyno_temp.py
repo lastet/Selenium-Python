a4dd1b71394e2e5d3d9f2547d4de3b0f30517818
Anna <104223316+lastet@users.noreply.github.com>
2022-06-08 18:14:36 -0500

    Add files via upload

---
+++ dyno_temp.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service(":\python projects\drivers\chromedriver.exe")
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument('disable-dev-shm-usage')
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://automated.pythonanywhere.com")
+    return driver
+
+def temp_only(text):
+    output = float(text.split(": ")[1])
+    return output
+
+def main():
+  driver = get_driver()
+  #element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  #//*[@id="displaytimer"]
+  #element = driver.find_element(by="id", value="displaytimer")
+  time.sleep(2)
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+  return temp_only(element.text)
+
+print(main())
