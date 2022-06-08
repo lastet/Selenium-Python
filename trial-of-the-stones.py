f8159b4f130e070f1b0dbc61c375a3d73de4dc8a
Anna <104223316+lastet@users.noreply.github.com>
2022-06-08 10:13:11 -0500

    Update trial-of-the-stones.py

--- trial-of-the-stones.py
+++ trial-of-the-stones.py
@@ -1,48 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
 
 browser = webdriver.Chrome(executable_path="C:\python projects\drivers\chromedriver.exe")
 
 url ="https://techstepacademy.com/trial-of-the-stones"
 browser.get(url)
-time.sleep(2)
+WebDriverWait(browser, 10).until(presence_of_element_located((By.CSS_SELECTOR, 'input[id="r1Input"]')))
 
 #a. Riddle of the Stone
 inptfield1 = browser.find_element(By.CSS_SELECTOR, 'input[id="r1Input"]')
 inptfield1.send_keys("rock")
-time.sleep(2)
+print("Answer 1 field is found")
 
 inptfield1btn = browser.find_element(By.XPATH, "//button[@name='r1Btn']")
 inptfield1btn.click()
-time.sleep(2)
+print("Answer 1 is in")
 
 firstriddleassert = browser.find_element(By.XPATH, "//h4[contains(text(),'bamboo')]").text
 
 #b. Riddle of Secrets
 inptfield2 = browser.find_element(By.CSS_SELECTOR, 'input[id="r2Input"]')
 inptfield2.send_keys(firstriddleassert)
-time.sleep(2)
+print("Answer 2 field is found")
 
 inptfield2btn = browser.find_element(By.XPATH, "//button[@name='r2Butn']")
 inptfield2btn.click()
-time.sleep(2)
+print("Answer 2 is in")
 
 #c. The Two Merchants
 richest_merchant = browser.find_element(By.XPATH,"//p[text()='3000']/../span").text
 
 inptfield3 = browser.find_element(By.CSS_SELECTOR, 'input[id="r3Input"]')
 inptfield3.send_keys(richest_merchant)
-time.sleep(2)
+print("Richest Merchant name is in")
 
 inptfield3btn = browser.find_element(By.XPATH, "//button[@name='r3Butn']")
 inptfield3btn.click()
-time.sleep(2)
+print("Answer 3 is in")
 
 
+#Complete button
 inptfield4 = browser.find_element(By.XPATH, "//button[@name='checkButn']")
 inptfield4.click()
-time.sleep(2)
+print("Complete button clicked")
 
 #assertion
 success = browser.find_element(By.XPATH, "//h4[contains(text(),'Trial Complete')]").text
@@ -51,5 +54,5 @@ if success == real_success:
     print("Great Success!")
 else:
     print("Try again.")
-time.sleep(2)
+
 browser.quit()
